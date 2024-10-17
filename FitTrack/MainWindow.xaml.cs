1612f2bcbccb8477bc796a5409ef0e3155d4cc16
ture-c <ture.christenson@edu.newton.se>
2024-10-17 09:14:42 +0200

    Test

--- FitTrack/MainWindow.xaml.cs
+++ FitTrack/MainWindow.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Shapes;
 
 namespace FitTrack
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
