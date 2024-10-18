0b3235bb94d34122d0001af3e7f67cb60f4bd718
ture-c <ture.christenson@edu.newton.se>
2024-10-18 20:32:02 +0200

    Tog bort mvvm ska försöka senare, byggde på add user/ registrera user.

--- FitTrack/MainWindow.xaml.cs
+++ FitTrack/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,13 +12,50 @@ using System.Windows.Shapes;
 
 namespace FitTrack
 {
+
     
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            DataContext=new MainWindowMVVM();
+            
         }
+
+        private void LogInbtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UserNameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "123")
+            {
+                //Ny sida
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+    
+
+        private void NewUserbtn_Click(object sender, RoutedEventArgs e)
+        {
+                    NewUserWindow newuserwindow = new NewUserWindow(); 
+
+                    newuserwindow.Show();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ForgotP_Click(object sender, RoutedEventArgs e)
+        {
+            //NewForgetPWindow forgotwin = new NewForgetPWindow();
+            //forgotwin.Show();
+
+        }
+        
     }
 }
---
+++ FitTrack/NewUserWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FitTrack
+{
+    /// <summary>
+    /// Interaction logic for NewUserWindow.xaml
+    /// </summary>
+    public partial class NewUserWindow : Window
+    {
+        private string userPassword = "";
+        public NewUserWindow()
+        {
+            InitializeComponent();
+            SelectCountries();
+        }
+
+        private void SelectCountries()
+        {
+            List<string> countryselect = new List<string>
+            {
+                "United States", "Canada",
+                "United Kingdom",
+                "France", "China",
+                "Norway",
+                "Sweden",
+                "Denmark",
+                "Germany",
+                "North Korea",
+                "Christmas Island",
+                "Burkina Faso",
+                "Yemen",
+                "Angola",
+                
+            };
+            CountryComboBox.ItemsSource = countryselect;
+            countryselect.Sort();
+
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            userPassword = passwordBox.Password; 
+        }
+
+        private void AddUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string password = userPassword;
+            string specialCharacters = "@\\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+            string numbers = "1234567890";
+
+            bool hasSpecial = false;
+            bool hasNumber = false;
+
+            
+            foreach (char c in password)
+            {
+                if (specialCharacters.Contains(c))
+                {
+                    hasSpecial = true;
+                    break;
+                }
+            }
+
+            
+            foreach (char c in password)
+            {
+                if (numbers.Contains(c))
+                {
+                    hasNumber = true;
+                    break;
+                }
+            }
+
+          
+            if (hasSpecial && hasNumber)
+            {
+                MessageBox.Show("Password is valid.");
+            }
+            else
+            {
+                MessageBox.Show("Password must contain at least one special character and one number.");
+            }
+
+
+
+        }
+    }
+}
