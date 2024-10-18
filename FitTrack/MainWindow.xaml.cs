ae2e880233d5f520212dcaa3e0ca364d65ffb390
ture-c <ture.christenson@edu.newton.se>
2024-10-18 10:02:33 +0200

    Gjort grunden i mvvm och wpf

--- FitTrack/MainWindow.xaml.cs
+++ FitTrack/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace FitTrack
         public MainWindow()
         {
             InitializeComponent();
+            DataContext=new MainWindowMVVM();
         }
     }
 }
---
+++ FitTrack/MainWindowMVVM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack
+{
+	class MainWindowMVVM : INotifyPropertyChanged
+	{
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void onPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+		private string username;
+
+		public string Username
+		{
+			get { return username; }
+			set 
+			{
+				username = value;
+				onPropertyChanged(nameof(Username));
+			}
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set 
+			{
+				password = value;
+				onPropertyChanged(nameof(Password));
+			
+			}
+		}
+
+
+	}
+}
